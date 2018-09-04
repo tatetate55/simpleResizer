3a04ec707b7b5887b975495cb9fbec318577416f
KAMAKURAKAZUHIRO <ruteinyoyokk@gmail.com>
2018-09-04 23:11:03 +0900

    初期状態だとダメなのとサイズおかしいのを修正

--- myResizer/DragView.swift
+++ myResizer/DragView.swift
@@ -56,15 +56,17 @@ class DragView: NSView {
         
         // 画像をドラッグ＆ドロップで読み込む例
         if let image = NSImage(pasteboard: sender.draggingPasteboard()) {
-            // ここで画像の処理を行う
-            let newImage: NSImage? = resize(sourceImage:image, newMaxSize: settingArray[0].maxSize)
-
-            let hoge = (draggedFileURL.deletingPathExtension?.lastPathComponent)! + settingArray[0].addFileName  + ".jpg"
-            let dirUrl = draggedFileURL.deletingLastPathComponent
-            let destinationURL = dirUrl?.appendingPathComponent(hoge)
-            
-            if (newImage?.jpegWrite(to: destinationURL!, options: .atomicWrite))! {
-                //print("File saved")
+            if  settingArray[0].maxSize > 0 || settingArray[0].addFileName  != "" {
+                // ここで画像の処理を行う
+                let newImage: NSImage? = resize(sourceImage:image, newMaxSize: settingArray[0].maxSize/2) //???TODO
+                
+                let hoge = (draggedFileURL.deletingPathExtension?.lastPathComponent)! + settingArray[0].addFileName  + ".jpg"
+                let dirUrl = draggedFileURL.deletingLastPathComponent
+                let destinationURL = dirUrl?.appendingPathComponent(hoge)
+                
+                if (newImage?.jpegWrite(to: destinationURL!, options: .atomicWrite))! {
+                    //print("File saved")
+                }
             }
         }
         
@@ -116,7 +118,7 @@ extension DragView { //後でファイルバラす
         
         sourceImage.size = newSize
         
-        NSGraphicsContext.current?.imageInterpolation = .high
+        NSGraphicsContext.current?.imageInterpolation = .medium
         sourceImage.draw(at: NSPoint.zero, from: CGRect(x: 0,y: 0, width: newSize.width, height: newSize.height), operation: NSCompositingOperation.copy, fraction: 1.0)
         newImage.unlockFocus()
         
--- myResizer/ViewController.swift
+++ myResizer/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet weak var imageNameTextField: NSTextField!
 
     // realm set
-    var settingData = SettingData()
     let realm = try! Realm()
+    var settingData = SettingData()
     var settingArray = try! Realm().objects(SettingData.self).sorted(byKeyPath: "id", ascending: false)
     
     //  let fromAppDelegate: AppDelegate = NSApplication.shared.delegate as! AppDelegate
@@ -25,27 +25,33 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
         maxSizeTextField.delegate = self
         imageNameTextField.delegate = self
         
         dragView.delegate = self
-
-        //settingArray[0].maxSize = 100
         
-        maxSizeTextField.placeholderString = settingArray[0].maxSize.description
-        imageNameTextField.placeholderString = settingArray[0].addFileName
+        //settingArray[0].maxSize = 100
+        if settingArray.isEmpty {
+            settingData.maxSize = 1000
+            settingData.addFileName = "hogehoge2"
+            try! realm.write {
+                self.realm.add(self.settingData, update: true)
+            }
+        }
+        maxSizeTextField.stringValue = settingArray[0].maxSize.description
+        imageNameTextField.stringValue = settingArray[0].addFileName
         
     }
     
     override func controlTextDidChange(_ obj: Notification) {
 
-        if imageNameTextField.stringValue != "" {
+      //  if imageNameTextField.stringValue != "" {
             try! realm.write {
                 settingData.addFileName = imageNameTextField.stringValue
                 self.realm.add(self.settingData, update: true)
             }
-
-        }
+        
         if let n = NumberFormatter().number(from: maxSizeTextField.stringValue) {
             try! realm.write {
                 settingData.maxSize = CGFloat(truncating: n)
